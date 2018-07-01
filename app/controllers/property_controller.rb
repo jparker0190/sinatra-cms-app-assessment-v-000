44c50146d1397e219ce82c737f91772efb988a50
Joseph Parker <jparker010190@gmail.com>
2018-07-01 00:10:21 +0000

    fixing user relationship bug

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -1,17 +1,45 @@
 class PropertyController < ApplicationController
-  get "/property" do
-    redirect_if_not_logged_in
-    @prop = Property.all
-    erb :'properties/index'
+  get '/property' do
+    if logged_in?
+      @prop = Property.all
+      erb :'properties/index'
+    else
+      redirect to '/login'
+    end
   end
 
-  get "/property/new" do
-    redirect_if_not_logged_in
-    @error_message = params[:error]
-    erb :'properties/new'
+  get '/property/new' do
+      if logged_in?
+        erb :'properties/new'
+      else
+        redirect to '/login'
+      end
+    end
+
+  post '/property' do
+    if logged_in?
+      if params[:name] == "" || params[:rooms] == ""
+        redirect to "/property/new"
+      else
+        @prop = current_user.propertys.build(name: params[:name], rooms: params[:rooms])
+        if @prop.save
+          redirect to "/property/#{@prop.id}"
+        else
+          redirect to "/property/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-  get "/property/:id/edit" do
+  get "/property/:id" do
+    redirect_if_not_logged_in
+    @prop = Property.find(params[:id])
+    erb :'properties/show'
+  end
+
+  get '/property/:id/edit' do
     redirect_if_not_logged_in
     @error_message = params[:error]
     @prop = Property.find(params[:id])
@@ -19,6 +47,16 @@ class PropertyController < ApplicationController
   end
 
   post "/property/:id" do
+   redirect_if_not_logged_in
+   @prop = Property.find(params[:id])
+   unless Property.valid_params?(params)
+     redirect "/property/#{@prop.id}/edit?error=invalid golf bag"
+   end
+   @prop.update(params.select{|k|k=="name" || k=="rooms"})
+   redirect "/property/#{@prop.id}"
+
+ end
+  patch '/property/:id' do
     redirect_if_not_logged_in
     @prop = Property.find(params[:id])
     unless Property.valid_params?(params)
@@ -28,19 +66,15 @@ class PropertyController < ApplicationController
     redirect "/property/#{@prop.id}"
   end
 
-  get "/property/:id" do
-    redirect_if_not_logged_in
-    @prop = Property.find_by_id(params[:id])
-    erb :'properties/show'
-  end
-
-  post "/property" do
-    redirect_if_not_logged_in
-
-    unless Property.valid_params?(params)
-      redirect "/property/new?error=invalid property"
+  delete '/property/:id/delete' do
+    if logged_in?
+      @prop = Property.find_by_id(params[:id])
+      if @prop && @prop.user == current_user
+        @prop.delete
+      end
+      redirect to '/property'
+    else
+      redirect to '/login'
     end
-    Property.create(params)
-    redirect "/property"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include Slugifiable::InstanceMethods
 	extend Slugifiable::ClassMethods
+  has_secure_password
   validates :username, uniqueness: true
   has_many :propertys
 end
